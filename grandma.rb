c142ec0aeed07c39d4c122e0b1874fdecbd960bb
upbeat-changelog-8230 <jkalu1000@gmail.com>
2019-04-30 17:10:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma_
     puts "HUH?! SPEAK UP, SONNY!"
   elsif speak_to_grandma_  == grandma".upcase
    puts "NO, NOT SINCE 1938!" 
-  else
+  elsif speak_to_grandma_
     puts "I LOVE YOU TOO PUMPKIN!"
 end
 
